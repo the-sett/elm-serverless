c90e6b25f4b67f43690696b0d7bc009344d2c7ac
Rupert Smith <rupert@thesett.com>
2019-07-17 11:31:19 +0100

    Added js logic to handle the random number ports.

--- demo/src/Interop/api.js
+++ demo/src/Interop/api.js
@@ -1,7 +1,19 @@
 const elmServerless = require('../../../src-bridge');
 
-const { Elm } = require('./API.elm');
+const {
+  Elm
+} = require('./API.elm');
 
+const app = Elm.Interop.API.init();
+
+// Random numbers through a port.
+if (app.ports != null && app.ports.requestRand != null) {
+  app.ports.requestRand.subscribe(() => {
+    app.ports.respondRand.send(Math.random());
+  });
+}
+
+// Create the serverless handler with the ports.
 module.exports.handler = elmServerless.httpApi({
-  app: Elm.Interop.API.init()
+  app
 });
